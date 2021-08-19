f5a03278826795688b0d880254893e89ef86993b
Mikko Saario <5684274+midisfi@users.noreply.github.com>
2021-08-19 17:25:24 +0300

    Create APIGatewayV2DomainNameConfigTLS.py
    draft

---
+++ APIGatewayV2DomainNameConfigTLS.py
@@ -0,0 +1,32 @@
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+
+class APIGatewayV2DomainNameTLSProtocol(BaseResourceCheck):
+    def __init__(self):
+        name = "Ensure API Gateway V2 SecurityPolicy is TLS v1.2"
+        id = "CUSTOM_APIGWV2_TLS_1.2"
+        supported_resources = ['AWS::ApiGatewayV2::DomainName']
+        categories = [CheckCategories.ENCRYPTION]
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf):
+        """
+            Ensures originProtocolPolicy for custom origins is TLS v1.2:
+                https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-customoriginconfig.html
+                https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesOriginSSLProtocols
+                https://docs.aws.amazon.com/cloudfront/latest/APIReference/API_OriginSslProtocols.html
+        :param conf: cloudfront configuration
+        :return: <CheckResult>
+        """
+
+        if 'Properties' in conf.keys():
+            if 'DistributionConfig' in conf['Properties'].keys():
+                if 'Origins' in conf['Properties']['DistributionConfig'].keys():
+                    for origin in range(len(conf['Properties']['DistributionConfig']['Origins'])):
+                        for item in range(len(conf['Properties']['DistributionConfig']['Origins'][origin]['customOriginConfig']['originSslProtocols']['items'])):
+                            protocol = conf['Properties']['DistributionConfig']['Origins'][origin]['customOriginConfig']['originSslProtocols']['items']
+                            if protocol and all(elem == 'TLSv1.2' for elem in protocol):
+                                return CheckResult.PASSED
+        return CheckResult.FAILED
+
+check = APIGatewayV2DomainNameTLSProtocol()
