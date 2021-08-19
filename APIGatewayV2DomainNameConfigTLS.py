b0182f05181fc8c6f88dc4f1877243f78937ffcf
Mikko Saario <5684274+midisfi@users.noreply.github.com>
2021-08-19 17:30:36 +0300

    Update APIGatewayV2DomainNameConfigTLS.py
    draft2

--- APIGatewayV2DomainNameConfigTLS.py
+++ APIGatewayV2DomainNameConfigTLS.py
@@ -3,7 +3,7 @@ from checkov.cloudformation.checks.resource.base_resource_check import BaseResou
 
 class APIGatewayV2DomainNameTLSProtocol(BaseResourceCheck):
     def __init__(self):
-        name = "Ensure API Gateway V2 SecurityPolicy is TLS v1.2"
+        name = "Ensure API Gateway V2 SecurityPolicy is TLS v1.2 or better"
         id = "CUSTOM_APIGWV2_TLS_1.2"
         supported_resources = ['AWS::ApiGatewayV2::DomainName']
         categories = [CheckCategories.ENCRYPTION]
@@ -11,11 +11,10 @@ class APIGatewayV2DomainNameTLSProtocol(BaseResourceCheck):
 
     def scan_resource_conf(self, conf):
         """
-            Ensures originProtocolPolicy for custom origins is TLS v1.2:
-                https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cloudfront-distribution-customoriginconfig.html
-                https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/distribution-web-values-specify.html#DownloadDistValuesOriginSSLProtocols
-                https://docs.aws.amazon.com/cloudfront/latest/APIReference/API_OriginSslProtocols.html
-        :param conf: cloudfront configuration
+            Ensures SecurityPolicy for custom domains is TLS v1.2 or better:
+                https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-apigatewayv2-domainname-domainnameconfiguration.html#cfn-apigatewayv2-domainname-domainnameconfiguration-securitypolicy
+                https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-custom-domain-tls-version.html
+        :param conf: apigwv2 configuration
         :return: <CheckResult>
         """
 
